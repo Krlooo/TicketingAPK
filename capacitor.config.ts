f6ec9ea37e44e10cdb676ab1e091761d225e7470
Karlo <karlomodz@gmail.com>
2025-03-20 20:29:58 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PushTest',
+  webDir: 'www'
+};
+
+export default config;
